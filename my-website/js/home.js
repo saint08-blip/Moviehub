65b2f53a7a937048c7569f4d241cddeab4617954
saint08-blip <marl.saint08@gmail.com>
2025-08-29 13:42:51 +0800

    Add files via upload

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = '13026b2882f03698bf0e0757cd0fbeeb';
+const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
@@ -119,6 +119,5 @@ const API_KEY = '13026b2882f03698bf0e0757cd0fbeeb';
       displayList(tvShows, 'tvshows-list');
       displayList(anime, 'anime-list');
     }
-
 
     init();
